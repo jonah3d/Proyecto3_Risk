ab6b8d590fe987216227f4be2844fb95b2e42703
jonah3d <84209915+jonah3d@users.noreply.github.com>
2025-05-21 13:11:09 +0200

    Taking out autofill

--- src/main/java/com/proyecto3/risk/controllers/sessioncontrollers/GameSession.java
+++ src/main/java/com/proyecto3/risk/controllers/sessioncontrollers/GameSession.java
@@ -161,7 +161,7 @@ public class GameSession {
         broadcast(gameStartMessage);
 
 
-        autoFillTerritories();
+       // autoFillTerritories();
 
 
         if (stage == GameStage.OCCUPATION) {
