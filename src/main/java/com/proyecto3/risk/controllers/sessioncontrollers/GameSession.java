1c2ca9f5db35aa23a9690fc2059c660347906267
jonah3d <84209915+jonah3d@users.noreply.github.com>
2025-05-21 23:04:56 +0200

    fortifying sin autofill v1

--- src/main/java/com/proyecto3/risk/controllers/sessioncontrollers/GameSession.java
+++ src/main/java/com/proyecto3/risk/controllers/sessioncontrollers/GameSession.java
@@ -366,7 +366,7 @@ public class GameSession {
             int currentTroops = sourceOccupy.getTroops();
             System.out.println("Source country has " + currentTroops + " troops");
 
-            if (currentTroops <= attackingTroops + 1) {
+            if (currentTroops - attackingTroops < 1) {
                 System.out.println("ERROR - Not enough troops (need to leave 1 behind)");
                 sendToPlayer(playerId, Map.of("action", "error", "message", "You must leave at least one troop behind."));
                 return;
